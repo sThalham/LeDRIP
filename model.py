5325455d9c947afcdc1a2e891d4c4247c1607a34
Your Name <you@example.com>
2022-06-03 22:17:16 +0200

    refine network

--- model.py
+++ model.py
@@ -13,15 +13,15 @@ class combinedModel(nn.Module):
     def __init__(self, input_size=(3, 128, 128), output_size=(3, 64, 64)):
         super(combinedModel, self).__init__()
         self.encoder = self.generate_CNN_encoder(input_size)
-        self.decoder = self.generate_CNN_decoder(output_size)
-        self.pose_head = self.generate_pnp(output_size)
+        #self.decoder = self.generate_CNN_decoder(output_size)
+        #self.pose_head = self.generate_pnp(output_size)
 
     def forward(self, x):
         bottleneck = self.encoder(x)
-        encoding = self.decoder(bottleneck)
-        pose = self.pose_head(encoding)
+        #encoding = self.decoder(bottleneck)
+        #pose = self.pose_head(encoding)
 
-        return bottleneck, encoding, pose
+        return bottleneck#, encoding, pose
 
     def generate_ViT_encoder(self, input_size):
         vit_b_16 = models.vit_b_16(pretrained=True)
@@ -40,14 +40,19 @@ class combinedModel(nn.Module):
     def generate_CNN_encoder(self, input_size):
         #input_size = input_size.cuda()
         rnxt50 = models.resnext50_32x4d(pretrained=True)
-        #print(rnxt50)
-        idx = 0
-        for name, param in rnxt50.named_parameters():
-            print(idx, ':', name, param.size())
-            idx += 1
-        #summary(rnxt50, input_size.to(device))
+        self.C1 = getattr(rnxt50, 'ReLU-3')
+        self.C2 = getattr(rnxt50, 'ReLU-39')
+        self.C3 = getattr(rnxt50, 'ReLU-81')
+        self.C4 = getattr(rnxt50, 'ReLU-143')
+        self.C5 = getattr(rnxt50, 'ReLU-171')
+        self.BN = getattr(rnxt50, 'AdaptiveAvgPool2d-173')
 
-        return rnxt50
+        #idx = 0
+        #for name, param in rnxt50.named_parameters():
+        #    print(idx, ':', name, param.size())
+        #    idx += 1
+
+        return C1, C2, C3, C4, C5, BN
 
     def generate_CNN_decoder(self, output_size):
         up1 = nn.ConvTranspose2d()
--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms, models
+from torchsummary import summary
 import numpy as np
 
 from model import combinedModel
@@ -13,7 +14,11 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 def main():
-    model = combinedModel().to(device)
+    model = combinedModel()#.to(device)
+    summary(model, (3, 128, 128), device="cpu")
+
+    x = torch.randn((8, 3, 128, 128)).to(device)
+    out = model(x)
 
 
 if __name__ == '__main__':
